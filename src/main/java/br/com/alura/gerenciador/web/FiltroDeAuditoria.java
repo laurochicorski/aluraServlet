e6268b4c31f8ed66133d11a56a234b134af9c420
lauro.chicorski <lauro.chicorski@JAVA35.sankhya.com.br>
2016-09-21 11:17:50 -0300

    finalizado aula 6

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -25,7 +25,7 @@ public class FiltroDeAuditoria implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		
-		Cookie cookie = getUsuario(req);
+		Cookie cookie = new Cookies(req.getCookies()).getUsuarioLogado();
 		String usuario = "<deslogado>";
 		
 		if(cookie != null){
@@ -42,20 +42,5 @@ public class FiltroDeAuditoria implements Filter{
 		// TODO Auto-generated method stub
 		
 	}
-	
-	private Cookie getUsuario(HttpServletRequest req){
-		Cookie[] cookies = req.getCookies();
 		
-		if(cookies != null){
-			for(Cookie cookie : cookies){
-				if(cookie.getName().equals("usuario.logado")){
-					return cookie;
-				}
-			}
-		}
-		
-		return null;
-	}
-
-	
 }

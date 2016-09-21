b715d988b59388a4e60a30f50f07bf3fb1e2296b
lauro.chicorski <lauro.chicorski@JAVA35.sankhya.com.br>
2016-09-21 10:59:31 -0300

    finalizado aula 5

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns="/*")
@@ -23,14 +24,37 @@ public class FiltroDeAuditoria implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
-		System.out.println("Usuário acessando a URI: " + req.getRequestURI());
-		chain.doFilter(request, response);
+		
+		Cookie cookie = getUsuario(req);
+		String usuario = "<deslogado>";
+		
+		if(cookie != null){
+			usuario = cookie.getValue();
+		
+			System.out.println("Usuário acessando a URI: " + req.getRequestURI());
+			chain.doFilter(request, response);
+		}
+		
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private Cookie getUsuario(HttpServletRequest req){
+		Cookie[] cookies = req.getCookies();
+		
+		if(cookies != null){
+			for(Cookie cookie : cookies){
+				if(cookie.getName().equals("usuario.logado")){
+					return cookie;
+				}
+			}
+		}
+		
+		return null;
 	}
 
 	
